9710572e7a13f916a139bd2ca7f8521ca4ad535d
Kero <2660936365@qq.com>
2021-02-04 01:13:47 +0800

    2021-02-04 01:13

---
+++ 2.4-SPI-9341-STM32测试程序-Touch/OBJ/startup_stm32f40_41xxx.d
@@ -0,0 +1,1 @@
+..\obj\startup_stm32f40_41xxx.o: ..\CMSIS\Source\arm\startup_stm32f40_41xxx.s
--- RT-Thread/Demo1/Project/Objects/main.d
+++ RT-Thread/Demo1/Project/Objects/main.d
@@ -1,11 +1,13 @@
-./objects/main.o: ..\User\main.c \
-  C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM4\Include\ARMCM4.h \
-  C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
-  C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h \
-  C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM4\Include\system_ARMCM4.h
+.\objects\main.o: ..\User\main.c
+.\objects\main.o: ..\rtthread\3.0.3\include\rtthread.h
+.\objects\main.o: ..\rtthread\3.0.3\include\rtdef.h
+.\objects\main.o: ..\User\rtconfig.h
+.\objects\main.o: ..\rtthread\3.0.3\include\rtservice.h
+.\objects\main.o: C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM4\Include\ARMCM4.h
+.\objects\main.o: C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: C:\Users\Kero\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\Device\ARM\ARMCM4\Include\system_ARMCM4.h
